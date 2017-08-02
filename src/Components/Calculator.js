bcaa0a355094215e428e0dbe90aadd914f44b782
Jason Brown <jasontbrown99@gmail.com>
2017-08-01 18:16:09 -0700

    add initial components

---
+++ src/Components/Calculator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BoilingVerdict from './BoilingVerdict';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Hello</div>
+    );
+  }
+}
+
+export default Calculator;
